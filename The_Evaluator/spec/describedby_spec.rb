81c1ed1557e842c97f5f626eb387151ccd4def4c
Mark Wilkinson <markw@illuminae.com>
2022-03-31 14:57:20 +0200

    new benchmark environment.  Fixed the HTTP headers for the citeas-describedby-item benchmark

--- The_Evaluator/spec/describedby_spec.rb
+++ The_Evaluator/spec/describedby_spec.rb
@@ -3,7 +3,7 @@ require 'open3'
 DescribedBy = String
 
 describe DescribedBy do 
-   context "When testing the described-by metric" do 
+   context "When testing the describedby metric" do 
       
     it "should fail to find random strings (test of the rspec test) for https://s11.no/2022/a2a-fair-metrics/01-http-describedby-only/" do 
         result, error, status = Open3.capture3('ruby ./tests/Apples_describedby "https://s11.no/2022/a2a-fair-metrics/01-http-describedby-only/"')
@@ -20,6 +20,10 @@ describe DescribedBy do
         expect(result.match(/SUCCESS/).class.to_s).to eq "MatchData"
     end
       
+    it "should return FAILURE for https://w3id.org/a2a-fair-metrics/11-http-described-iri-wrong-type/ where the link is an IRI" do 
+        result, error, status = Open3.capture3('ruby ./tests/Apples_describedby "https://w3id.org/a2a-fair-metrics/11-http-described-iri-wrong-type/"')
+        expect(result.match(/FAILURE/).class.to_s).to eq "MatchData"
+    end
    end
 
 end
