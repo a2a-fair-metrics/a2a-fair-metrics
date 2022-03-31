a3b9e7b53d7352b6702e34eaa2cfea42e5d18c91
Mark Wilkinson <markw@illuminae.com>
2022-03-31 13:17:57 +0200

    added another reference environment and got rspec working for the Evaluator tests

--- The_Evaluator/apples-tests.gemspec
+++ The_Evaluator/apples-tests.gemspec
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "tests"
+#require_relative "tests"
 
 Gem::Specification.new do |spec|
   spec.name = "apples-tests"
--- The_Evaluator/spec/perma-cite-as_spec.rb
+++ The_Evaluator/spec/perma-cite-as_spec.rb
@@ -3,7 +3,7 @@ require 'open3'
 CiteAs = String
 
 describe CiteAs do 
-   context "When testing the cite-as metric" do 
+   context "When testing the perma-cite-as metric" do 
       
     it "should fail to find random strings (test of the rspec test) for https://w3id.org/a2a-fair-metrics/03-http-citeas-only/" do 
         result, error, status = Open3.capture3('ruby ./tests/Apples_cite-as "https://w3id.org/a2a-fair-metrics/03-http-citeas-only/"')
@@ -25,6 +25,10 @@ describe CiteAs do
         expect(result.match(/SUCCESS/).class.to_s).to eq "MatchData"
     end
 
+    it "should return FAILURE for https://s11.no/2022/a2a-fair-metrics/08-http-citeas-not-perma/" do 
+        result, error, status = Open3.capture3('ruby ./tests/Apples_cite-as "https://s11.no/2022/a2a-fair-metrics/08-http-citeas-not-perma/"')
+        expect(result.match(/FAILURE/).class.to_s).to eq "MatchData"
+    end
       
    end
 
