9edd6f72d48653439371db7ada0a8b3eab589ee8
Mark Wilkinson <markw@illuminae.com>
2022-07-20 11:05:52 +0200

    cleaning and moving around

--- The_Evaluator/tests/web_utils.rb
+++ The_Evaluator/tests/web_utils.rb
@@ -1,43 +1,40 @@
-def fetch(url, headers = ACCEPT_ALL_HEADER)  #we will try to retrieve turtle whenever possible
+def fetch(url, headers = ACCEPT_ALL_HEADER)  # we will try to retrieve turtle whenever possible
+  warn 'In fetch routine now.  '
 
-    $stderr.puts "In fetch routine now.  "
-    
-    begin
-        $stderr.puts "executing call over the Web to #{url.to_s}"
-        response = RestClient::Request.execute({
-                method: :get,
-                url: url.to_s,
-                #user: user,
-                #password: pass,
-                headers: headers})
-        if @meta
-            @meta.finalURI |= [response.request.url]
-        end
-        $stderr.puts "There was a response to the call #{url.to_s}"
-        $stderr.puts "Response code #{response.code}"
-        if response.code == 203 
-            @meta.comments << "WARN: Response is non-authoritative (HTTP response code: #{response.code}).  Headers may have been manipulated encountered when trying to resolve #{url.to_s}\n" if @meta
-        end
-        return [response.headers, response.body]
-    rescue RestClient::ExceptionWithResponse => e
-        $stderr.puts "EXCEPTION WITH RESPONSE! #{e.response}\n#{e.response.headers}"
-        @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url.to_s}\n" if @meta
-        if e.response.code == 500
-            response = [false, false]
-        else
-            response = [e.response.headers, e.response.body]
-        end
-        return response  # now we are returning the headers and body that were returned
-    rescue RestClient::Exception => e
-        $stderr.puts "EXCEPTION WITH NO RESPONSE! #{e}"
-        @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url.to_s}\n" if @meta
-        response = [false, false]
-        return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
-    rescue Exception => e
-        $stderr.puts "EXCEPTION UNKNOWN! #{e}"
-        @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url.to_s}\n" if @meta
-        response = [false, false]
-        return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
-    end		  # you can capture the Exception and do something useful with it!\n",
-
+  begin
+    warn "executing call over the Web to #{url}"
+    response = RestClient::Request.execute({
+                                             method: :get,
+                                             url: url.to_s,
+                                             # user: user,
+                                             # password: pass,
+                                             headers: headers
+                                           })
+    @meta.finalURI |= [response.request.url] if @meta  # it's possible to call this method without affecting the metadata object being created by the harvester
+    warn "There was a response to the call #{url}"
+    warn "Response code #{response.code}"
+    if response.code == 203 && @meta
+      @meta.comments << "WARN: Response is non-authoritative (HTTP response code: #{response.code}).  Headers may have been manipulated encountered when trying to resolve #{url}\n"
+    end
+    [response.headers, response.body]
+  rescue RestClient::ExceptionWithResponse => e
+    warn "EXCEPTION WITH RESPONSE! #{e.response}\n#{e.response.headers}"
+    @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url}\n" if @meta
+    if e.response.code == 500
+      [false, false]
+    else
+      [e.response.headers, e.response.body]
+    end
+    # now we are returning the headers and body that were returned
+  rescue RestClient::Exception => e
+    warn "EXCEPTION WITH NO RESPONSE! #{e}"
+    @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url}\n" if @meta
+    [false, false]
+    # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  rescue Exception => e
+    warn "EXCEPTION UNKNOWN! #{e}"
+    @meta.comments << "WARN: HTTP error #{e} encountered when trying to resolve #{url}\n" if @meta
+    [false, false]
+    # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  end
 end
